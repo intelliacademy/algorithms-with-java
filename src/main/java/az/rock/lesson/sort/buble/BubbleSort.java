0e5dc4bee9a9ff4c4d905821fae86a7baa21668f
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-03-08 18:41:09 +0400

    Insertion Sort

--- src/main/java/az/rock/lesson/sort/buble/BubbleSort.java
+++ src/main/java/az/rock/lesson/sort/buble/BubbleSort.java
@@ -3,12 +3,23 @@ package az.rock.lesson.sort.buble;
 import az.rock.lesson.util.AbstractTimeMetric;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class BubbleSort  <T extends Comparable<T> & Cloneable> extends AbstractTimeMetric<T> {
 
     @Override
     public List<T> sort(List<T> list) {
-        return null;
+        int n = list.size();
+        IntStream.range(0,n).forEach(iIndex->{
+            IntStream.range(1,n-iIndex).forEach(jIndex->{
+                if (list.get(jIndex-1).compareTo(list.get(jIndex)) > 0){
+                    var temp = list.get(jIndex);
+                    list.set(jIndex,list.get(jIndex-1));
+                    list.set(jIndex-1,temp);
+                }
+            });
+        });
+        return list;
     }
 
 }
